4fa81f0620b38c4272f2e53072b0b49d210954ab
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-08-04 12:15:30 -0700

    added blog post model outline

--- models/BlogPost.js
+++ models/BlogPost.js
@@ -1,3 +1,43 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.STRING
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                //'id' from User
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blogPost'
+    }
+);
+
+module.exports=BlogPost;
