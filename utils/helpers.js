4f0fcacff48517f50348d7577ed78aea4c4b19f5
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-08-04 14:35:24 -0700

    added rest of handlebars views and partials

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,7 +2,7 @@ module.exports = {
     //returns first 30 characters from blog content to show in summary card
     short_str: (str)=>{
     if (str.length > 30)
-      return str.substring(0,30);
+      return str.substring(0,30)+ '...';
     return str;
     },
     //formats date as mm/dd/yyy
