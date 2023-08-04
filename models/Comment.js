b5477f9429f8741ed55f43b71f2395617e6b1907
Miranda Brown <rawnda@Mirandas-MacBook-Pro.local>
2023-08-04 12:23:49 -0700

    added comment model outline, edited User model

--- models/Comment.js
+++ models/Comment.js
@@ -1,3 +1,44 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.STRING
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        blog_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                //'id' from Blog model
+            }
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                //'id' from User
+            }
+        }
+    }, {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment'
+    }
+);
+
+module.exports = Comment;
--- models/User.js
+++ models/User.js
@@ -16,17 +16,10 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    name: {
+    username: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
+      unique: true
     },
     password: {
       type: DataTypes.STRING,
@@ -41,6 +34,10 @@ User.init(
       beforeCreate: async (newUserData) => {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       },
     },
     sequelize,
